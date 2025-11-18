43457e17b2babeb151e0756db470834e8775f56b
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-18 15:29:12 +0100

    Update esercizio militare

---
+++ corso python/11_18_Martedì/Esercizi/Esercizio_ereditarieta_3.py
@@ -0,0 +1,12 @@
+from export.Unita import *
+from export.ControlloMilitare import *
+
+fanteria = Fanteria("fanteria", 10, "fucili")
+
+artiglieria = Artiglieria("artiglieria", 2, 120)
+
+cavalleria = Cavalleria("cavalleria", 6, 9)
+
+supporto_logistico = SupportoLogistico("supporto", 5, 14)
+
+ricognizione = Ricognizione("ricognizione", 10, 3)
--- corso python/11_18_Martedì/Esercizi/export/ControlloMilitare.py
+++ corso python/11_18_Martedì/Esercizi/export/ControlloMilitare.py
@@ -1,6 +1,7 @@
 from Unita import *
 
 class ControlloMilitare(Fanteria, Artiglieria, Cavalleria, SupportoLogistico, Ricognizione):
+    #prende tutti i parametri delle classi padre più una nuova
     def __init__(self, nome, numero_soldati, armi, gittata, velocita, quantita_materiali, durata_missione, unita_registrate):
         super().__init__(nome, numero_soldati, armi)
         Artiglieria.__init__(nome, numero_soldati, gittata)
@@ -8,3 +9,16 @@ class ControlloMilitare(Fanteria, Artiglieria, Cavalleria, SupportoLogistico, Ri
         SupportoLogistico.__init__(nome, numero_soldati, quantita_materiali)
         Ricognizione.__init__(nome, numero_soldati, durata_missione)
         self.unita_registrate = unita_registrate
+        
+    #splitta l'unita creando una lista di liste
+    def registra_unita(self, unita):
+        return self.unita_registrate.append(*unita)
+    
+    def mostra_unita(self):
+        return self.unita_registrate
+    
+    #itera la lista per trovare il nome
+    def dettagli_unita(self, nome):
+        for specific_unit in self.unita_registrate:
+            if specific_unit[0] == nome:
+                return specific_unit
