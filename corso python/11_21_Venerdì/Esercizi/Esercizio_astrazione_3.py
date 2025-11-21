b2cc1b0811095ad663677c11a9ac5766bfc6b32f
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-21 10:00:41 +0100

    Esercizio astrazione 3

---
+++ corso python/11_21_Venerdì/Esercizi/Esercizio_astrazione_3.py
@@ -0,0 +1,67 @@
+from export.Veicolo import *
+from export.GestoreFlotta import *
+
+def main():
+    
+    list_veicoli = []
+    
+    gestore = GestoreFlotta(list_veicoli)
+    
+    camion = Camion("Df433k", 100, 4)
+    
+    print(camion.carica(150))
+    
+    print(camion._carico_attuale)
+    
+    camion.scarica()
+    
+    print(camion._carico_attuale)
+    
+    gestore.aggiungi_veicolo(camion)
+    
+       
+    furgone = Furgone("Se230po", 150, "diesel")
+    
+    print(furgone.carica(50))
+    
+    print(furgone._carico_attuale)
+    
+    furgone.scarica()
+    
+    print(furgone._carico_attuale)
+    
+    gestore.aggiungi_veicolo(furgone)
+    
+    
+    motocarro = Motocarro("As700kk", 150, 10)
+    
+    print(motocarro.carica(50))
+    
+    print(motocarro._carico_attuale)
+    
+    motocarro.scarica()
+    
+    print(motocarro._carico_attuale)
+    
+    gestore.aggiungi_veicolo(motocarro)
+    
+    costo = gestore.costo_totale_manutenzione()
+    
+    print(f"Il costo di manutenzione totale è: {costo}")
+    
+    print()
+    
+    gestore.stampa_veicoli()
+    
+    print()
+    
+    gestore.rimuovi_veicolo("Se230po")
+    
+    gestore.stampa_veicoli()
+    
+    
+                
+if __name__ == "__main__":
+    main()
+else: 
+    print("È stato importato")
---
+++ corso python/11_21_Venerdì/Esercizi/export/VeicoloTrasporto.py
@@ -0,0 +1,26 @@
+from abc import ABC,abstractmethod
+
+#classe astratta che prende in input una stringa e due interi
+class VeicoloTrasporto(ABC):
+    #carico_attuale ha un valore di default di 0
+    def __init__(self, targa:str, peso_massimo:int, carico_attuale:int = 0):
+        self._targa = targa
+        self._peso_massimo = peso_massimo
+        self._carico_attuale = carico_attuale
+    
+    #da implementare nelle classi filgie
+    @abstractmethod
+    def costo_manutenzione(self):
+        pass
+
+    #controlla se è possibile aggiungere il valore int per poi aggiornare l'attributo carico_atttuale
+    def carica(self, peso:int):
+        self._carico_attuale += peso
+        if self._carico_attuale <= self._peso_massimo:
+            return f"È stato caricato, il carico attuale è: {self._carico_attuale} kg su {self._peso_massimo} kg massimi"
+        else:
+            return f"Non può essere inserito perchè supera il carico massimo di {self._peso_massimo} kg"
+    
+    #imposta a 0 il carico_attuale    
+    def scarica(self):
+        self._carico_attuale = 0
