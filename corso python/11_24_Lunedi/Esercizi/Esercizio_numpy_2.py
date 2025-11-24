d131ca37d64912b0ddbe3a7c5d7971d93bed3ffa
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-24 16:23:44 +0100

    Update Esercizio_numpy_2.py

--- corso python/11_24_Lunedi/Esercizi/Esercizio_numpy_2.py
+++ corso python/11_24_Lunedi/Esercizi/Esercizio_numpy_2.py
@@ -19,3 +19,22 @@ print("Ogni terzo elemento dell'array: ", slice_4)
 #assegna un valore nelle posizioni corrispondenti
 rand_arr[5:10] = 99
 print("L'array con i valori modificati dalla posizione 5 a 10 escluso: ", rand_arr)
+
+#array multidimensionale a partire da quella originale
+rand_reshaped = rand_arr.reshape(5, 4)
+print("Array multidimensionale: ", rand_reshaped)
+
+slice_1 = rand_reshaped[:, :2]
+print("Primi due colonne: ", slice_1)
+
+slice_2 = rand_reshaped[:, -2:]
+print("Ultime due colonne: ", slice_2)
+
+slice_3 = rand_reshaped[1:3, 2:3]
+print("Seconda e terza riga, terza colonna: ", slice_3)
+
+slice_4 = rand_reshaped[::2, ::2]
+print("Ogni due righe e colonne: ", slice_4)
+
+rand_reshaped[1:3, 2:3] = 10
+print("Nuova array multidimensionale: ", rand_reshaped)
