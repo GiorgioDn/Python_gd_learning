9eb0bdd8c82e1ade7c1c5a8ba4abdab1943781c0
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-24 14:04:44 +0100

    Create Esempio_numpy.py

---
+++ corso python/11_24_Lunedi/Esempi/Esempio_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#creazione di un array unidimensionale
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+#creazione di un array bidimensionale
+arr2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr2d)
