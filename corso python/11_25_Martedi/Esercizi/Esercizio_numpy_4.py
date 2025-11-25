0e04fb1e0b50d6e168f7958c2e2257c52c952817
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-25 12:43:16 +0100

    Esercizio manipolazione array multidimensionale

--- corso python/11_25_Martedi/Esercizi/Esercizio_numpy_4.py
+++ corso python/11_25_Martedi/Esercizi/Esercizio_numpy_4.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+#matrice 5x5 con sequenza di numeri
+matrix = np.arange(1,26).reshape(5, 5)
+print("Matrice: ", matrix)
+
+#seconda colonna della matrice
+second_col = matrix[: , 1:2]
+print("Seconda colonna della matrice: ", second_col)
+
+#terza riga della matrice
+third_row = matrix[2:3]
+print("Terza riga della matrice: ", third_row)
+
+#somma della diagonale principale
+sum_diag = np.diag(matrix).sum()
+print("La somma della diagonale principale è: ", sum_diag)
