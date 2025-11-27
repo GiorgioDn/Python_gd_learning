f33f3de2026de785c1d4e3572eb3383270f22f86
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-27 14:35:49 +0100

    Update Esercizio_padas_2.py

--- corso python/11_27_Giovedi/Esercizi/Esercizio_padas_2.py
+++ corso python/11_27_Giovedi/Esercizi/Esercizio_padas_2.py
@@ -21,6 +21,29 @@ print(result)
 #prodotto più venduto
 print("\n=====PRODOTTO CON MAGGIORI VENDITE=====\n")
 max_sell = df.groupby("Prodotto")["Quantita"].sum()
+#restituisce il valore prodotto corrispondente al valore massimo di quantita
 max_sell_id = max_sell.idxmax()
 max_quantity = max_sell.max()
 print(max_sell_id, max_quantity)
+
+#città con maggiori vendite totali
+print("\n=====CITTA' CON MAGGIORI VENDITE TOTALI=====\n")
+max_sell_city = df.groupby("Città")["Totale vendite"].sum()
+max_sell_id_city = max_sell_city.idxmax()
+max_tot_city = max_sell_city.max()
+print(max_sell_id_city, max_tot_city)
+
+#dataframe con vendite superiori ad un valore
+print("\n=====VENDITE FILTRATE=====\n")
+df_2 = df[df["Totale vendite"]>5000]
+print(df_2)
+
+#ordinare colonne totale vendite in ordine decrescente
+print("\n=====COLONNE TOTALE VENDITE IN ORDINE DECRESCENTE=====\n")
+df = df.sort_values(by="Totale vendite", ascending=False)
+print(df)
+
+#visualizzare totali vendite per ogni città
+print("\n=====TOTALE VENDITE PER OGNI CITTA'=====\n")
+tot_city = df.groupby("Città")["Totale vendite"].sum()
+print(tot_city)
